b0ed76ba8884c64dab01a288c44c9d40793de38c
xilin <hzxilin@gmail.com>
2025-08-10 15:08:48 +0800

    Update settings.py

--- weibo/settings.py
+++ weibo/settings.py
@@ -7,12 +7,12 @@ COOKIES_ENABLED = False
 TELNETCONSOLE_ENABLED = False
 LOG_LEVEL = 'ERROR'
 # 访问完一个页面再访问下一个时需要等待的时间，默认为10秒
-DOWNLOAD_DELAY = 10
+DOWNLOAD_DELAY = 0.5
 DEFAULT_REQUEST_HEADERS = {
     'Accept':
     'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7',
-    'cookie': 'your cookie'
+    'cookie': 'XSRF-TOKEN=zxuPP5FAFtMlLD4ezkJgC-up; SCF=AsOtqcwz4oylqVIAW_wt7uLHQI0xFJ6LhxbXKnR_6yP9Vv2Qsb5kvAN3G_GSOlQt_2B-Nhr4I_ND7wFBE09PfX4.; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W53dkTRZo1x_2cqjOTZRS-35JpX5KzhUgL.FoM0S0-XSo5feKe2dJLoIEXLxKMLBKnL1h5LxK.LBK-LB-BLxKnLBK-LB.qLxKqL1-eLBKnLxKqL1-eLBKnt; WBPSESS=bE-DSIeJx2-KcY3Ei0BWA5lA6L9uB41QpkuEeZQI-V_wmnJPD4YjSmYwQ8ViPsfa9WkKUDKRYkl4FeKn8sv5bLn4w9L9usgctbEAbdWRjHW2cZID0u0YqHlSSErUHCzUVXAtrMvwAtv_1kBjNoKdMQ==; SUB=_2A25KfdX-DeRhGeFN7FcV9i7Jyj-IHXVp81c2rDV8PUNbmtANLXSmkW1NQ82zFAlyVd7VZJV_Sa-yzGnMWAQRH91w; ALF=02_1738617518; _s_tentry=weibo.com; Apache=3705100167139.059.1736051504374; SINAGLOBAL=3705100167139.059.1736051504374; ULV=1736051504399:1:1:1:3705100167139.059.1736051504374:'
 }
 ITEM_PIPELINES = {
     'weibo.pipelines.DuplicatesPipeline': 300,
@@ -24,7 +24,7 @@ ITEM_PIPELINES = {
 }
 # 要搜索的关键词列表，可写多个, 值可以是由关键词或话题组成的列表，也可以是包含关键词的txt文件路径，
 # 如'keyword_list.txt'，txt文件中每个关键词占一行
-KEYWORD_LIST = ['迪丽热巴']  # 或者 KEYWORD_LIST = 'keyword_list.txt'
+KEYWORD_LIST = ['美国']  # 或者 KEYWORD_LIST = 'keyword_list.txt'
 # 要搜索的微博类型，0代表搜索全部微博，1代表搜索全部原创微博，2代表热门微博，3代表关注人微博，4代表认证用户微博，5代表媒体微博，6代表观点微博
 WEIBO_TYPE = 1
 # 筛选结果微博中必需包含的内容，0代表不筛选，获取全部微博，1代表搜索包含图片的微博，2代表包含视频的微博，3代表包含音乐的微博，4代表包含短链接的微博
@@ -38,7 +38,7 @@ START_DATE = '2020-03-01'
 END_DATE = '2020-03-01'
 # 进一步细分搜索的阈值，若结果页数大于等于该值，则认为结果没有完全展示，细分搜索条件重新搜索以获取更多微博。数值越大速度越快，也越有可能漏掉微博；数值越小速度越慢，获取的微博就越多。
 # 建议数值大小设置在40到50之间。
-FURTHER_THRESHOLD = 46
+FURTHER_THRESHOLD = 30
 # 图片文件存储路径
 IMAGES_STORE = './'
 # 视频文件存储路径
